ef250f923801449af425b7ba94415891796d96b4
Sung <sung272012@hotmail.com>
2024-10-16 15:23:54 -0400

    Fetch Fake Store API

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,12 +1,30 @@
 import Navbar from '../components/Navbar';
 import Category from '../components/Category';
+import { useState, useEffect } from 'react';
 
 export default function Shop() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+      .then((res) => res.json())
+      .then((json) => setProducts(json));
+  });
+
   return (
     <>
       <h1>This is Shop Page!</h1>
       <Navbar />
       <Category />
+      <ul>
+        {products.map((product) => {
+          return (
+            <>
+              <li key={product.id}>{product.title}</li>
+            </>
+          );
+        })}
+      </ul>
     </>
   );
 }
